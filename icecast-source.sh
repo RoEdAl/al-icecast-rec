368017109ccda5fd8172fd23476d480cbcf0e54d
Edmunt Pienkowsky <roed@onet.eu>
2018-06-13 13:28:31 +0200

    OGG stream - packet-per-page
    Set queue-size for each mount individually.
    
    icecast-source - use arrays.

--- icecast-source.sh
+++ icecast-source.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -e
 
-# commands
-readonly ARECORD_CMD="arecord -q -M -N -t raw -D ${AUDIO_DEVICE:-hw:} -c 2 -r ${AUDIO_SAMPLE_RATE:-48000} -f ${ARECORD_FORMAT:-S16_LE}"
-readonly FFMPEG_PRE="ffmpeg -hide_banner -loglevel repeat+error -f ${FFMPEG_FORMAT:-s16le} -ac 2 -ar ${AUDIO_SAMPLE_RATE:-48000} -i -"
+readonly -a ARECORD_PARAMS=(-q -M -N -t raw -D ${AUDIO_DEVICE:-hw:} -c 2 -r ${AUDIO_SAMPLE_RATE:-48000} -f ${ARECORD_FORMAT:-S16_LE})
+readonly -a FFMPEG_PRE=(-hide_banner -loglevel repeat+error -f ${FFMPEG_FORMAT:-s16le} -ac 2 -ar ${AUDIO_SAMPLE_RATE:-48000} -i -)
+readonly -a OPUS_PARAMS=(-b:a ${OPUS_BITRATE:-96k} -compression_level 0 -vbr constrained -packet_loss ${OPUS_PACKET_LOSS:-5} -frame_duration 10)
+readonly -a FLAC_PARAMS=(-compression_level 0 -lpc_coeff_precision 1 -lpc_type none -lpc_passes 1 -prediction_order_method estimation -ch_mode indep)
 
-# params
-readonly OPUS_PARAMS="-b:a ${OPUS_BITRATE:-96k} -vbr constrained -packet_loss ${OPUS_PACKET_LOSS:-5}"
-readonly FLAC_PARAMS='-compression_level 0 -lpc_coeff_precision 1 -lpc_type none -lpc_passes 1 -prediction_order_method estimation -ch_mode indep'
+readonly SERIAL_OPUS=100
+readonly SERIAL_FLAC=101
 
-declare -r -a ICE_SOURCE=(
+readonly -a ICE_SOURCE=(
     -content_type 'application/ogg'
     -ice_name "${SOURCE_NAME:-Live Stream}"
     -ice_description "${SOURCE_DESCRIPTION:-Opus codec}"
     "icecast://source:${PWD_SOURCE:-source}@[::1]:${SERVER_PORT:-50000}/live")
 
-declare -r -a HD_ICE_SOURCE=(
+readonly -a HD_ICE_SOURCE=(
     -content_type 'application/ogg'
     -ice_name "${HD_SOURCE_NAME:-Live Stream HD}"
     -ice_description "${HD_SOURCE_DESCRIPTION:-FLAC codec}"
@@ -22,18 +22,18 @@ declare -r -a HD_ICE_SOURCE=(
 
 case $1 in
     opus)
-    $ARECORD_CMD | $FFMPEG_PRE -f ogg -c:a libopus $OPUS_PARAMS "${ICE_SOURCE[@]}"
+    arecord "${ARECORD_PARAMS[@]}" | ffmpeg "${FFMPEG_PRE[@]}" -f ogg -page_duration 1 -serial_offset $SERIAL_OPUS -c:a libopus "${OPUS_PARAMS[@]}" "${ICE_SOURCE[@]}"
     ;;
 
     flac)
-    $ARECORD_CMD | $FFMPEG_PRE -f ogg -c:a flac $FLAC_PARAMS "${HD_ICE_SOURCE[@]}"
+    arecord "${ARECORD_PARAMS[@]}" | ffmpeg "${FFMPEG_PRE[@]}" -f ogg -page_duration 1 -serial_offset $SERIAL_FLAC -c:a flac "${FLAC_PARAMS[@]}" "${HD_ICE_SOURCE[@]}"
     ;;
 
     both)
-    $ARECORD_CMD | $FFMPEG_PRE \
+    arecord "${ARECORD_PARAMS[@]}" | ffmpeg "${FFMPEG_PRE[@]}" \
         -filter_complex '[0:a] asplit=2 [in1][iin2]; [iin2] aformat=sample_fmts=s16 [in2]' \
-        -map '[in1]' -f ogg -c:a libopus $OPUS_PARAMS "${ICE_SOURCE[@]}" \
-        -map '[in2]' -f ogg -c:a flac $FLAC_PARAMS "${HD_ICE_SOURCE[@]}"
+        -map '[in1]' -f ogg -page_duration 1 -serial_offset $SERIAL_OPUS -c:a libopus "${OPUS_PARAMS[@]}" "${ICE_SOURCE[@]}" \
+        -map '[in2]' -f ogg -page_duration 1 -serial_offset $SERIAL_FLAC -c:a flac "${FLAC_PARAMS[@]}" "${HD_ICE_SOURCE[@]}"
     ;;
 
     *)
